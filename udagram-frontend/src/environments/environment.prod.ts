f85bd7374350749857c491daf71bd88837307d63
AhmdNabil <94638729+AhmdNabil@users.noreply.github.com>
2022-04-15 06:20:00 +0200

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-vmpvh3yd.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-kx5zsszt.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
